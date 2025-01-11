2844d587495f4902f7c7339a376819ab5efba6f5
Dhruv Baliyan <dhruvbaliyan1001@gmail.com>
2025-01-11 14:32:51 +0530

    fix: ignore broken links

--- sidebars.ts
+++ sidebars.ts
@@ -19,7 +19,7 @@ const sidebars: SidebarsConfig = {
   // But you can create a sidebar manually
   
   tutorialSidebar: [
-    'intro',
+    // 'intro',
     // 'hello',
     {
       type: 'category',
@@ -29,7 +29,7 @@ const sidebars: SidebarsConfig = {
     {
       type: 'category',
       label: 'Backend',
-      items: ['backend/intro', 'backend/NL-APIs-Documentation', 'backend/Student-and-Spot-Assessment-Data', 'backend/Assessment-Cycle-Management', 'backend/Bulk-Examiner-Ingestion'],
+      items: ['backend/NL-APIs-Documentation', 'backend/Student-and-Spot-Assessment-Data', 'backend/Assessment-Cycle-Management', 'backend/Bulk-Examiner-Ingestion'],
     },
     {
       type: 'category',
